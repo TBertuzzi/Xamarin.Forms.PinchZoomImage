d38bf9f1c34d49494926db0dd2acb26a62baf005
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2018-09-20 10:19:37 -0300

    :tada: First Commit

---
+++ Xamarin.Forms.PinchZoomImage/PinchZoom.cs
@@ -0,0 +1,135 @@
+﻿using System;
+namespace Xamarin.Forms.PinchZoomImage
+{
+
+    public class PinchZoom : ContentView
+    {
+        double currentScale = 1;
+        double startScale = 1;
+        double xOffset = 0;
+        double yOffset = 0;
+
+        public PinchZoom()
+        {
+            PinchGestureRecognizer pinchGesture = new PinchGestureRecognizer();
+            pinchGesture.PinchUpdated += PinchUpdated;
+            GestureRecognizers.Add(pinchGesture);
+
+            var panGesture = new PanGestureRecognizer();
+            panGesture.PanUpdated += OnPanUpdated;
+            GestureRecognizers.Add(panGesture);
+        }
+
+        private void PinchUpdated(object sender, PinchGestureUpdatedEventArgs e)
+        {
+            if (e.Status == GestureStatus.Started)
+            {
+     
+                startScale = Content.Scale;
+                Content.AnchorX = 0;
+                Content.AnchorY = 0;
+            }
+
+            if (e.Status == GestureStatus.Running)
+            {
+                currentScale += (e.Scale - 1) * startScale;
+                currentScale = Math.Max(1, currentScale);
+
+                double renderedX = Content.X + xOffset;
+                double deltaX = renderedX / Width;
+                double deltaWidth = Width / (Content.Width * startScale);
+                double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+
+                double renderedY = Content.Y + yOffset;
+                double deltaY = renderedY / Height;
+                double deltaHeight = Height / (Content.Height * startScale);
+                double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+
+                double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
+                double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
+
+                Content.TranslationX = Math.Min(0, Math.Max(targetX, -Content.Width * (currentScale - 1)));//  targetX.Clamp(-Content.Width * (currentScale - 1), 0);
+                Content.TranslationY = Math.Min(0, Math.Max(targetY, -Content.Height * (currentScale - 1)));  // targetY.Clamp(-Content.Height * (currentScale - 1), 0);
+
+                Content.Scale = currentScale;
+            }
+
+            if (e.Status == GestureStatus.Completed)
+            {
+                xOffset = Content.TranslationX;
+                yOffset = Content.TranslationY;
+            }
+        }
+
+        public void OnPanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+            if (Content.Scale == 1)
+            {
+                return;
+            }
+
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+
+                    double newX = (e.TotalX * Scale) + xOffset;
+                    double newY = (e.TotalY * Scale) + yOffset;
+
+                    double width = (Content.Width * Content.Scale);
+                    double height = (Content.Height * Content.Scale);
+
+                    bool canMoveX = width > Application.Current.MainPage.Width;
+                    bool canMoveY = height > Application.Current.MainPage.Height;
+
+                    if (canMoveX)
+                    {
+                        double minX = (width - (Application.Current.MainPage.Width / 2)) * -1;
+                        double maxX = Math.Min(Application.Current.MainPage.Width / 2, width / 2);
+
+                        if (newX < minX)
+                        {
+                            newX = minX;
+                        }
+
+                        if (newX > maxX)
+                        {
+                            newX = maxX;
+                        }
+                    }
+                    else
+                    {
+                        newX = 0;
+                    }
+
+                    if (canMoveY)
+                    {
+                        double minY = (height - (Application.Current.MainPage.Height / 2)) * -1;
+                        double maxY = Math.Min(Application.Current.MainPage.Width / 2, height / 2);
+
+                        if (newY < minY)
+                        {
+                            newY = minY;
+                        }
+
+                        if (newY > maxY)
+                        {
+                            newY = maxY;
+                        }
+                    }
+                    else
+                    {
+                        newY = 0;
+                    }
+
+                    Content.TranslationX = newX;
+                    Content.TranslationY = newY;
+                    break;
+                case GestureStatus.Completed:
+                    xOffset = Content.TranslationX;
+                    yOffset = Content.TranslationY;
+                    break;
+            }
+        }
+    }
+
+}
